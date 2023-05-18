857cbf15856fb643dde53e71b7b251ea932e3810
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-18 07:41:53 -0800

    feat: area method and init for rooms

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,13 @@
 class Room
 
-  def initialize
+  attr_reader :category
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width.to_s
+  end
 
+  def area
+    @length * @width.to_i
   end
 end
