1fd34eaf389bcb54a2192cc554a4cfce95a4314c
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-18 08:11:36 -0800

    feat: add room method and test syntax

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -4,27 +4,29 @@ require './lib/house'
 RSpec.describe House do
   describe "Iteration 2" do
 
-    before(:each) do
-      house = House.new("$400000", "123 sugar lane")
+    before :each do
+      @house = House.new("$400000", "123 sugar lane")
     end
 
     it "exists" do
-      expect(house).to be_a House
+      expect(@house).to be_a House
     end
 
     it "has a price" do
-      expect(house.price).to eq("$400000")
+      expect(@house.price).to eq("$400000")
     end
 
     it "has an address" do
-      expect(house.address).to eq("123 sugar lane")
+      expect(@house.address).to eq("123 sugar lane")
     end
 
     it "can be given rooms" do
-      expect(house.rooms).to eq([])
+      expect(@house.rooms).to eq([])
       room_1 = Room.new(:bedroom, 10, '13')
       room_2 = Room.new(:bedroom, 11, '15')
-      expect(house.rooms).to eq([room_1, room_2])
+      @house.add_room(room_1)
+      @house.add_room(room_2)
+      expect(@house.rooms).to eq([room_1, room_2])
     end
 
   end
